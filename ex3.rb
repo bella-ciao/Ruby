caefc207dedead504e4eba146f90bae50817d69c
Lilian <lmunno@lmunno.local>
2014-02-23 13:04:12 -0500

    adding the 3rd exercise with some extras

---
+++ ex3.rb
@@ -0,0 +1,43 @@
+# adding some comments in code accordingly to exercise guide
+puts "I will now count my chickens:"
+
+# this is to calculate the amount of roosters and hens
+puts "Hens", 25 + 30 / 6
+puts "Roosters", 100 - 25 * 3 % 4
+
+# This part is to count eggs
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# In this part of the code, we will test some hypothesis
+puts "Is it true that 3 + 2 < 5?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater?", 5 > 2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
+
+# Doing some random caculations now
+
+puts "I want to count and check my vinyls now"
+puts "12 inches", 25 * 3
+puts "7 inches", 20 - 10 * 4
+puts "10 inches", 1 + 1
+
+puts "Do I have more 12 inches than 7 inches?"
+puts 25 * 3 > 20 - 10 * 4
+
+puts "Do I have more 7 inches than 10 inches?"
+puts 10 * 4 > 1 + 1
+
+puts "Do I have more 10 inches than 12 inches?"
+puts 25 * 3 < 1 + 1
