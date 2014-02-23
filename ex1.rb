0ec7ccb603029dc86d3c1f3a68e7d92eedc70de9
Lilian <lmunno@lmunno.local>
2014-02-23 11:45:22 -0500

    adding extras

--- ex1.rb
+++ ex1.rb
@@ -2,6 +2,8 @@ puts "Hello World!"
 puts "Hello Again"
 puts "I like typing this"
 puts "This is fun"
-puts 'Yay! Printing'
+puts "Yay! Printing"
 puts "I'd much rather 'not'."
-puts 'I "said" do not touch this.' 
+puts "I said do not touch this."
+puts "I want to print another line!"
+puts 'I want to print only this line'
