3ba9e5a16f4b33e7fa85dfbf5519108877823bf1
Lilian <lmunno@lmunno.local>
2014-02-23 12:17:48 -0500

    adding exercise 2

---
+++ ex2.rb
@@ -0,0 +1,6 @@
+# this is the exercise to add comments on code
+# we do that using octothorpes
+
+puts "I already did this exercise." # but didn't have on my github
+# puts "but this line won't run"
+puts "This one will." 
